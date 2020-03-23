f64bcb045809042e9dd605ccca55f28f8d525526
Mendy Danzinger <mendy.danzinger@unity3d.com>
2020-03-23 00:01:46 -0400

    fix: add start_time as basicstat stat, fix tests

--- plugins/serializers/ubazzar/ubazzar_json_test.go
+++ plugins/serializers/ubazzar/ubazzar_json_test.go
@@ -1,31 +1,52 @@
 package ubazzar
 
 import (
-	"fmt"
-	"github.com/influxdata/telegraf/metric"
-	"github.com/stretchr/testify/assert"
+	"encoding/json"
 	"testing"
 	"time"
+
+	"github.com/influxdata/telegraf/metric"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestSerializeMetricFloat(t *testing.T) {
+	// Setup test metric
 	now := time.Now()
 	tags := map[string]string{
-		"event_id" : "test",
-		"customer_id": "testCustomer",
+		"customer_id":     "testCustomer",
 		"unit_of_measure": "na-net-gb",
 	}
 	fields := map[string]interface{}{
-		"quantity": 25.0,
+		"quantity":   25.0,
 		"start_time": now.Add(time.Second * -30).Format(time.RFC3339),
 	}
 	m, err := metric.New("net", tags, fields, now)
 	assert.NoError(t, err)
 
+	// Serialize Metric
 	s, _ := NewSerializer(0)
 	var buf []byte
 	buf, err = s.Serialize(m)
 	assert.NoError(t, err)
-	expS := []byte(fmt.Sprintf(`{"event_id":"test","service_customer_id":"testCustomer","service":"-","unit_of_measure":"na-net-gb","quantity":25,"start_time":"%s","end_time":"%s"}`, now.Add(time.Second * -30).Format(time.RFC3339), now.Format(time.RFC3339)) + "\n")
-	assert.Equal(t, string(expS), string(buf))
+
+	// Get UUID from serialized metric
+	ubazzarEvent := &event{}
+	err = json.Unmarshal(buf, ubazzarEvent)
+	assert.NoError(t, err)
+
+	expectedMetric := event{
+		EventID:           ubazzarEvent.EventID,
+		ServiceCustomerID: "testCustomer",
+		Service:           "",
+		UnitOfMeasure:     "na-net-gb",
+		Quantity:          25.0,
+		StartTime:         now.Add(time.Second * -30).Format(time.RFC3339),
+		EndTime:           now.Format(time.RFC3339),
+		MetaData:          make(map[string]string),
+	}
+
+	exp, err := json.Marshal(expectedMetric)
+	assert.NoError(t, err)
+
+	assert.Equal(t, string(exp)+"\n", string(buf))
 }
